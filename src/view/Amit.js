a7e2926d0fa3d20b9644e28bb1f15a8d9faf7bc5
Amit <amitz2507@gmail.com>
2018-03-04 19:44:55 +0200

    Amit Page

--- src/view/Amit.js
+++ src/view/Amit.js
@@ -3,6 +3,7 @@ import m from 'mithril';
 var Amit = {
     view: function () {
         return m('div', [
+            m('h1', "Amit's Header"),
             m('p', "Amit's component"),
             m('p', "Amit's Second component")
         ])
