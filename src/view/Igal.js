5e7ccfe6c39c2b7f145ca22fdac21981bc3d205e
Tal Yaron <tal.yaron@gmail.com>
2018-03-04 19:49:28 +0200

    some change

--- src/view/Igal.js
+++ src/view/Igal.js
@@ -2,7 +2,7 @@ import m from 'mithril';
 
 var Igal = {
     view: function () {
-        return m('div', [m('p', "Igal's component edited by Adam")])
+        return m('div', [m('p', "Igal's component edited by Adam and Tal")])
     }
 }
 
