f6aa19d68befca2aae4d1f281af73cb64480522a
Adam Zagri <adam@rotlein.co.il>
2018-03-04 19:45:51 +0200

    adam

--- src/view/Adam.js
+++ src/view/Adam.js
@@ -3,8 +3,7 @@ import m from 'mithril';
 var Adam = {
     view: function () {
         return m('div', [
-            m('p', "Adam's component"),
-            m('p', "Adam's Second component")
+            m('h4', "ADAM ZAGRI")
         ])
     }
 }
